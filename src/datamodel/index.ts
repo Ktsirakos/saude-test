a7332957b6b914f1ad17daf79ab39195e7096861
Konstantinos Tsirakos <tkonstantinos1996@hotmail.com>
2021-08-18 16:33:05 +0300

    Added datamodel for person entry

---
+++ src/datamodel/index.ts
@@ -0,0 +1,79 @@
+class Serialized {
+  private identifier: string;
+  constructor(_id: string) {
+    this.identifier = _id;
+  }
+
+  public id(): string {
+    return this.identifier;
+  }
+}
+
+/**
+"_id": "5d5d7ad6570814f5f612116e",
+"age": 39,
+"eyeColor": "green",
+"name": "Holman Preston",
+"gender": "male",
+"location": {
+  "latitude": -11.533498,
+  "longitude": 168.340816
+},
+"preferences": {
+  "pet": "dog",
+  "fruit": "strawberry"
+}
+*/
+
+export enum Gender {
+  MALE = 0,
+  FEMALE,
+}
+
+export interface Location {
+  latitude: string;
+  longitude: string;
+}
+
+export interface Preferences {
+  pet: string;
+  fruit: string;
+}
+
+export interface PersonJSON {
+  _id: string;
+  age: number;
+  eyeColor: string;
+  name: string;
+  gender: "male" | "female";
+  location: Location;
+  preferences: Preferences;
+}
+
+export class Person extends Serialized {
+  constructor(
+    _id: string,
+    public name: string,
+    public age: number,
+    public gender: Gender,
+    public location: Location,
+    public preferences: Preferences
+  ) {
+    super(_id);
+  }
+
+  fromJSON(data: PersonJSON): Person {
+    return new Person(
+      data._id,
+      data.name,
+      data.age,
+      data.gender === "male" ? Gender.MALE : Gender.FEMALE,
+      data.location,
+      data.preferences
+    );
+  }
+
+  fromPersonJSONArray(data: PersonJSON[]): Person[] {
+    return data.map((elem: PersonJSON) => Person.prototype.fromJSON(elem));
+  }
+}
